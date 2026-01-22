d2919784841c5508ecfa368dad3201c25a9702a8
Zi Lin <kowzilin@gmail.com>
2026-01-22 18:45:10 +0800

    feat(transaction): add transaction routes
    - Add API endpoint for create and read transactions

--- backend/app/api/deps/common.py
+++ backend/app/api/deps/common.py
@@ -7,10 +7,12 @@ from app.db.database import get_db
 from app.repositories.account import AccountRepository
 from app.repositories.auth import AuthRepository
 from app.repositories.category import CategoryRepository
+from app.repositories.transaction import TransactionRepository
 from app.repositories.user import UserRepository
 from app.services.account import AccountService
 from app.services.auth import AuthService
 from app.services.category import CategoryService
+from app.services.transaction import TransactionService
 from app.services.user import UserService
 
 
@@ -38,3 +40,16 @@ def get_account_service(db: Annotated[Session, Depends(get_db)]) -> AccountServi
 def get_category_service(db: Annotated[Session, Depends(get_db)]) -> CategoryService:
     category_repo = CategoryRepository(db)
     return CategoryService(category_repo)
+
+
+def get_transaction_service(
+    db: Annotated[Session, Depends(get_db)],
+) -> TransactionService:
+    transaction_repo = TransactionRepository(db)
+    account_repo = AccountRepository(db)
+    category_repo = CategoryRepository(db)
+
+    account_service = AccountService(account_repo)
+    category_service = CategoryService(category_repo)
+
+    return TransactionService(transaction_repo, account_service, category_service)
--- backend/app/api/router.py
+++ backend/app/api/router.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter
 
-from app.api.routes import account, auth, category, user
+from app.api.routes import account, auth, category, transaction, user
 
 api_router = APIRouter()
 
@@ -8,3 +8,4 @@ api_router.include_router(user.router)
 api_router.include_router(auth.router)
 api_router.include_router(account.router)
 api_router.include_router(category.router)
+api_router.include_router(transaction.router)
